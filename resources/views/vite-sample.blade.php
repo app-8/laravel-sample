2d2d01ac1e2ce3c3ea59cfc53d40427121d7b30a
engineer-yone3 <enginner.yone3@gmail.com>
2026-04-13 01:02:27 +0900

    feat: Nginx + PHP-FPM構成のLaravelプロジェクトを作成 / Create Laravel project with Nginx + PHP-FPM setup

---
+++ resources/views/vite-sample.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vite Sample</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Vite Sample Page</h1>
+    <p>このページは @@vite ディレクティブを使ってアセットをロードしています。</p>
+</body>
+</html>
